258e6648a13b49dee0eee46a4ace9242d36de804
C.B.C <chasechen609@gmail.com>
2025-08-01 23:58:55 -0700

    Update constants.ts

--- frontend/src/utils/constants.ts
+++ frontend/src/utils/constants.ts
@@ -82,10 +82,4 @@ export const TEST_ACCOUNTS = [
   { username: 'lawyer1', password: 'legal123', email: 'lawyer1@lawfirm.com', role: 'user', subscription_tier: 'premium' }
 ];
 
-
-DEFAULT_BACKEND_URL = "https://day-sons-grown-sides.trycloudflare.com";
-
-
-
-
-
+export const DEFAULT_BACKEND_URL = "https://day-sons-grown-sides.trycloudflare.com";
