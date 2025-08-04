edfa98e9c7161802be0a1433fb2c6a7663990e9b
C.B.C <chasechen609@gmail.com>
2025-08-04 00:14:14 -0700

    Update __init__.py

--- legal_assistant/services/__init__.py
+++ legal_assistant/services/__init__.py
@@ -10,12 +10,17 @@ from .rag_service import (
 )
 from .analysis_service import ComprehensiveAnalysisProcessor
 from .ai_service import call_openrouter_api
-from .external_db_service import (
-    LegalDatabaseInterface,
-    LexisNexisInterface,
-    WestlawInterface,
-    search_external_databases
-)
+
+# Import only what exists in external_db_service
+try:
+    from .external_db_service import (
+        search_external_databases,
+        search_free_legal_databases,
+        search_free_legal_databases_enhanced,
+        get_database_status
+    )
+except ImportError as e:
+    print(f"Warning: Some external database functions not available: {e}")
 
 __all__ = [
     'SafeDocumentProcessor',
@@ -29,9 +34,8 @@ __all__ = [
     'calculate_confidence_score',
     'ComprehensiveAnalysisProcessor',
     'call_openrouter_api',
-    'LegalDatabaseInterface',
-    'LexisNexisInterface',
-    'WestlawInterface',
-    'search_external_databases'
+    'search_external_databases',
+    'search_free_legal_databases',
+    'search_free_legal_databases_enhanced',
+    'get_database_status'
 ]
-
